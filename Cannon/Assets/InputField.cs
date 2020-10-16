30a11835e7e0804f13add68c70c67072816f0d60
Philip Huang <philip.sw.huang@gmail.com>
2020-10-16 16:26:50 -0700

    First commit

---
+++ Cannon/Assets/InputField.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InputField : MonoBehaviour {
+
+	string inputString = "";
+	int lengthOf = 4;
+
+	void Start () {
+
+	}
+
+	void OnGUI(){
+		inputString = GUILayout.TextField (inputString, lengthOf);
+	}
+
+	void Update () {
+		PlayerPrefs.SetString ("input", inputString);
+	}
+}
